760a3bfbeca04ec0127ed4737ca939a0843e80a3
FelixBlet <lolovtony@gmail.com>
2023-07-06 03:37:05 +0700

    add music command now u can listen music from youtube! also i change little things, but its dont matter

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ const { token } = require('./config.json');
 
 
 // Create a new client instance
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({ intents: [
+	GatewayIntentBits.Guilds,
+	GatewayIntentBits.GuildVoiceStates] });
 client.commands = new Collection();
 
 const commandsPath = path.join(__dirname, 'commands');
